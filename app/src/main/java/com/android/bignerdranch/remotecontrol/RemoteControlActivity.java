5a04ac4d62066bda67f8d29092aca414d826f8e6
C. Chang <conradc88@yahoo.com>
2015-05-15 10:54:58 +0800

    commit

--- app/src/main/java/com/android/bignerdranch/remotecontrol/RemoteControlActivity.java
+++ app/src/main/java/com/android/bignerdranch/remotecontrol/RemoteControlActivity.java
@@ -1,38 +1,20 @@
 package com.android.bignerdranch.remotecontrol;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
 
 
-public class RemoteControlActivity extends ActionBarActivity {
+public class RemoteControlActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_remote_control);
+    protected Fragment createFragment() {
+        return new RemoteControlFragment();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_remote_control, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onCreate(Bundle savedIntanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedIntanceState);
     }
 }
